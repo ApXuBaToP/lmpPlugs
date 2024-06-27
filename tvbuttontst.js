818f34c35c638a80be3bf460a063f67a6de47899
ApXuBaToP <47154696+ApXuBaToP@users.noreply.github.com>
2024-06-27 14:49:58 +0200

    Update tvbuttontst.js

--- tvbuttontst.js
+++ tvbuttontst.js
@@ -6,29 +6,29 @@
         }
   log('TVkeys', 'TVkeys keys 001 loaded');
   
-  var htmlx = '<div>\n \
-     <div class="simple-button simple-button--filter selector filter--home">\n \
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">\n \
-           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>\n \
-        </svg>\n \
-        <span>Home</span>\n \
-        <div class="hide"></div>\n \
-     </div>\n \
-     <div class="simple-button simple-button--filter selector filter--search">\n \
-        <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">\n \
-           <circle cx="9.9964" cy="9.63489" r="8.43556" stroke="currentColor" stroke-width="2.4"/>\n \
-           <path d="M20.7768 20.4334L18.2135 17.8701" stroke="currentColor" stroke-width="2.5" stroke-linecap="round"/>\n \
-        </svg>\n \
-        <div class="hide"></div>\n \
-     </div>\n \
-     <div class="simple-button simple-button--filter selector filter--sort">\n \
-        <span>#{filter_sorted}</span>\n \
-        <div class="hide"></div>\n \
-     </div>\n \
-     <div class="simple-button simple-button--filter selector filter--filter">\n \
-        <span>#{filter_filtred}</span>\n \
-        <div class="hide"></div>\n \
-     </div>\n \
+  var htmlx = '<div> \n \
+     <div class="simple-button simple-button--filter selector filter--home"> \n \
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16"> \n \
+           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/> \n \
+        </svg> \n \
+        <span>Home</span> \n \
+        <div class="hide"></div> \n \
+     </div> \n \
+     <div class="simple-button simple-button--filter selector filter--search"> \n \
+        <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg"> \n \
+           <circle cx="9.9964" cy="9.63489" r="8.43556" stroke="currentColor" stroke-width="2.4"/> \n \
+           <path d="M20.7768 20.4334L18.2135 17.8701" stroke="currentColor" stroke-width="2.5" stroke-linecap="round"/> \n \
+        </svg> \n \
+        <div class="hide"></div> \n \
+     </div> \n \
+     <div class="simple-button simple-button--filter selector filter--sort"> \n \
+        <span>#{filter_sorted}</span> \n \
+        <div class="hide"></div> \n \
+     </div> \n \
+     <div class="simple-button simple-button--filter selector filter--filter"> \n \
+        <span>#{filter_filtred}</span> \n \
+        <div class="hide"></div> \n \
+     </div> \n \
   </div>'
   
   var styles = `
