ebf3f6b427a957d5b13f52930c5f5ec5fcaf9933
apxubatop <apxubatop@gmail.com>
2024-09-24 19:49:50 +0200

    2

--- ippl.js
+++ ippl.js
@@ -4,25 +4,21 @@
       var text = lgs;
       console.log.apply(console.log, ['lmpPlugs', '[builtinPlList]: ' + lgs])
   }
-    log('Plugin loaded');
-    //Lampa.Controller._controllers = {};
-    //var contrList = ['player','player_panel','player_rewind'];
+    //log('Plugin loaded');
   
     function toggleListener(ev) {
       if (ev.name == 'player') setController(ev)
       }
   
     function setController(ev) {
-      log(ev.name + ' intercepted');
+      //log(ev.name + ' intercepted');
       var lp = Lampa.Controller.enabled().controller
-      //var lp = Lampa.Controller._controllers;
       if (!lp.patched) {
-          //lp[ev.name] = Lampa.Controller.enabled().controller;
-          lp.up = Lampa.PlayerPanel.listener.send('playlist')
+          lp.up = function() {Lampa.PlayerPanel.listener.send('playlist')}
           lp.patched = true
       }
   }
     Lampa.Controller.listener.follow('toggle', toggleListener);
-    log('Listener started');
+    //log('Listener started');
   
   })();
